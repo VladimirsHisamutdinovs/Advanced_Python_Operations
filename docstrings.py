144ec4bed909af74dd8fd61b370c221c0109660a
Vladimirs Hisamutdinovs <91381107+VladimirsHisamutdinovs@users.noreply.github.com>
2022-02-14 16:43:42 +0000

    Add files via upload
    adds docstrings.py

---
+++ docstrings.py
@@ -0,0 +1,21 @@
+def my_function(arg1, arg2=None):
+    """
+    my_function(arg1, arg2=None) --> no purpose function
+
+    Parameters:
+    arg1: first argument, any type
+    arg2: second argument, defaults to None
+    """
+    print(arg1, arg2)
+
+
+
+
+
+
+def main():
+    print(my_function.__doc__)
+    
+
+if __name__ == "__main__":
+    main()
