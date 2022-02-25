c8360f4a729364f9eb594c7962d50893cd85f5cb
Vladimirs Hisamutdinovs <91381107+VladimirsHisamutdinovs@users.noreply.github.com>
2022-02-25 12:49:21 +0000

    Add files via upload
    adds random_examples.py

---
+++ random_examples.py
@@ -0,0 +1,33 @@
+import random
+
+# print(random.random())
+# print(random.random())
+# print(random.random())
+
+# for i in range(10):
+#     if (random.random() <= 0.5):
+#         print("Heads")
+#     else:
+#         print("Tails")
+
+# print(random.uniform(1, 100))
+
+# print(random.randint(1, 100))
+
+# print(random.randrange(0, 101, 5))
+
+random.seed(1)
+print(random.random())
+print(random.random())
+
+print("=========================")
+
+random.seed(1)
+print(random.random())
+print(random.random())
+
+print("=========================")
+
+random.seed(1)
+print(random.random())
+print(random.random())
