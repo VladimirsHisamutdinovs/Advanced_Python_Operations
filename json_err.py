53760c88abf142536dbe54cf7862fb3f0f6412f5
Vladimirs Hisamutdinovs <91381107+VladimirsHisamutdinovs@users.noreply.github.com>
2022-03-13 15:09:35 +0000

    Add files via upload
    adds json_err.py

---
+++ json_err.py
@@ -0,0 +1,34 @@
+import json
+
+def main():
+
+    json_str = ''' {
+        "make" : "Opel",
+        "model" : "Mokka",
+        "colours" : [
+            "Black", 
+            "White",
+            "Blue"
+        ],
+        "price" : 20000.99
+    }'''
+
+    data = json.loads(json_str)
+
+    print("Car: " + data['make'])
+    if (data['model'] == 'Mokka'):
+        print('It\'s Mokka!')
+    for colour in data['colours']:
+        print('Colour: ' + colour)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
